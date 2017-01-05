33c16d1535dcecd84f37af7343025015024c99c6
agm1984 <agm1984@gmail.com>
2017-01-05 13:15:16 -0800

    contact form stuff

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 
+// Define routes
 var index = require('./routes/index');
 var about = require('./routes/about');
 var contact = require('./routes/contact');
@@ -20,7 +21,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ routes/contact.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET contact page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.post('/send', function(req, res, next){
+  var transporter = nodemailer.createTransport({
+    name: 'localhost'
+  });
+
+  var mailOptions = {
+    from: 'Admin <adam@adammackintosh.net>',
+    to: 'agm1984@gmail.com',
+    subject: 'Website Contact Submission',
+    text: 'You have a new submission with the following details...Name: '+req.body.name+' Email: '+req.body.email+' Message: '+req.body.message,
+    html: '<p>You have a new submittion with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      console.log('Message Sent: ' +info.response);
+      console.log(req.query);
+    }
+  });
+});
+
+module.exports = router;
